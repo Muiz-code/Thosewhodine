75ab4b7b146c44e4d3762b0176aed70d408714a3
muiz owolabi <beasteye.official@gmail.com>
2025-08-27 12:20:11 +0100

    fixed

--- src/Components/HoverCard.tsx
+++ src/Components/HoverCard.tsx
@@ -26,7 +26,7 @@ const FoodieCardWithHoverDescription: React.FC<CardProps> = ({
       {/* Image Section - takes up 50% of the card */}
       <div className="relative md:w-1/2 w-[100%] md:h-auto h-[30vh] flex-shrink-0 overflow-hidden">
         <img
-          src={ staticImage}
+          src={staticImage}
           alt={altText}
           className={`absolute inset-0 w-full h-full object-cover transition-transform duration-500 ease-in-out ${
             isHovered ? "scale-110" : "scale-100"
@@ -38,7 +38,7 @@ const FoodieCardWithHoverDescription: React.FC<CardProps> = ({
       {/* Description Section - This section takes up the remaining 50% */}
       <div className="md:w-1/2 w-[100%] flex-shrink-0 md:p-6 p-2 flex flex-col justify-center relative">
         <div
-          className={`absolute md:left-0 left-1 inset-0 flex flex-col md:items-center items-start justify-end pb-3 transition-opacity duration-300 ${
+          className={`absolute md:left-0 left-1 inset-0 flex flex-col items-center md:text-start text-center md:justify-end justify-center pb-3 transition-opacity duration-300 ${
             isHovered ? "opacity-0" : "opacity-100"
           }`}
         >
